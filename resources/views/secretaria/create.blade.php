41641c28d8f94f30c58b289010d8a2aecb7748c4
Tulio <tuliocll@gmail.com>
2020-05-14 11:58:46 -0300

    merge e corrigir blade secretaria

--- resources/views/secretaria/create.blade.php
+++ resources/views/secretaria/create.blade.php
@@ -34,38 +34,38 @@
                             <a href="{{ URL::to('secretaria') }}" class="btn btn-block btn-outline-info "><i class="fa fa-list-alt"></i> Listar Secretarias</a>
                         </div>
                     </div>
-                </div>
-                <div class="alert alert-danger erros" style="display: none;">
-                    <strong>Atenção!</strong> Houve algum problema com as suas informações.<br><br>
-                    <ul></ul>
-                </div>
-                <form method="POST" action="/secretaria" id="form">
-                    (<span style="color: red;">*</span>) Campos Obrigatórios
-                    <br><br>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="form-group col-6">
-                                <strong>Titulo</strong>
-                                <input type="text" autocomplete="off" id="titulo" name="titulo" class="form-control" value="">
-                            </div>
-                            <div class="form-group col-6">
-                                <strong>Sigla</strong>
-                                <input type="text" autocomplete="off" id="sigla" name="sigla" class="form-control" value="">
-                            </div>
-                        </div>
-                        <!-- /.card-body -->
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-info float-right salvar" data-loading-text="<i class='fa fa-circle-o-notch fa-spin'></i>
-                    &nbsp Aguarde...">Salvar</button>
-                        </div>
-                        <!-- /.card-footer -->
-                </form>
-            </div>
-            <!-- /.card -->
 
-        </div>
-</div>
-</section>
+                    <div class="alert alert-danger erros" style="display: none;">
+                        <strong>Atenção!</strong> Houve algum problema com as suas informações.<br><br>
+                        <ul></ul>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="/secretaria" id="form">
+                            (<span style="color: red;">*</span>) Campos Obrigatórios
+                            <br><br>
+                            <div class="row">
+                                <div class="form-group col-6">
+                                    <strong>Titulo</strong>
+                                    <input type="text" autocomplete="off" id="titulo" name="titulo" class="form-control" value="">
+                                </div>
+                                <div class="form-group col-6">
+                                    <strong>Sigla</strong>
+                                    <input type="text" autocomplete="off" id="sigla" name="sigla" class="form-control" value="">
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-info float-right salvar" data-loading-text="<i class='fa fa-circle-o-notch fa-spin'></i>
+                    &nbsp Aguarde...">Salvar</button>
+                            </div>
+                            <!-- /.card-footer -->
+                        </form>
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+            </div>
+    </section>
 </div>
 
 @endsection
--- resources/views/secretaria/edit.blade.php
+++ resources/views/secretaria/edit.blade.php
@@ -37,37 +37,37 @@
                             <a href="{{ URL::to('secretaria.index') }}" class="btn btn-block btn-outline-info "><i class="fa fa-list-alt"></i> Listar Secretarias</a>
                         </div>
                     </div>
-                </div>
-                <div class="alert alert-danger erros" style="display: none;">
-                    <strong>Atenção!</strong> Houve algum problema com as suas informações.<br><br>
-                    <ul></ul>
-                </div>
-                <form method="POST" action="/secretaria/{{$secretaria->id}}" id="form">
-                    (<span style="color: red;">*</span>) Campos Obrigatórios
-                    <br><br>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="form-group col-6">
-                                <strong>Titulo</strong>
-                                <input value='{{$secretaria->titulo}}' type="text" autocomplete="off" id="titulo" name="titulo" class="form-control">
-                            </div>
-                            <div class="form-group col-6">
-                                <strong>Sigla</strong>
-                                <input value='{{$secretaria->sigla}}' type="text" autocomplete="off" id="sigla" name="sigla" class="form-control">
-                            </div>
-                        </div>
-                        <!-- /.card-body -->
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-info float-right salvar" data-loading-text="<i class='fa fa-circle-o-notch fa-spin'></i>
-                    &nbsp Aguarde...">Salvar</button>
-                        </div>
-                        <!-- /.card-footer -->
-                </form>
-            </div>
-            <!-- /.card -->
 
+                    <div class="alert alert-danger erros" style="display: none;">
+                        <strong>Atenção!</strong> Houve algum problema com as suas informações.<br><br>
+                        <ul></ul>
+                    </div>
+                    <form method="POST" action="/secretaria/{{$secretaria->id}}" id="form">
+                        (<span style="color: red;">*</span>) Campos Obrigatórios
+                        <br><br>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="form-group col-6">
+                                    <strong>Titulo</strong>
+                                    <input value='{{$secretaria->titulo}}' type="text" autocomplete="off" id="titulo" name="titulo" class="form-control">
+                                </div>
+                                <div class="form-group col-6">
+                                    <strong>Sigla</strong>
+                                    <input value='{{$secretaria->sigla}}' type="text" autocomplete="off" id="sigla" name="sigla" class="form-control">
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-info float-right salvar" data-loading-text="<i class='fa fa-circle-o-notch fa-spin'></i>
+                    &nbsp Aguarde...">Salvar</button>
+                            </div>
+                            <!-- /.card-footer -->
+                    </form>
+                </div>
+                <!-- /.card -->
+
+            </div>
         </div>
-</div>
-</section>
+    </section>
 </div>
 @endsection
