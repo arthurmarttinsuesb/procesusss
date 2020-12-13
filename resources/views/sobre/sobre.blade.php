30d090669c3d15f02a23f398fd65c39f650b5a53
ronald dos santos matos <ronaldmatos76@gmail.com>
2020-12-13 17:13:37 -0300

    mudancas sobre

--- resources/views/sobre/sobre.blade.php
+++ resources/views/sobre/sobre.blade.php
@@ -25,95 +25,175 @@
     <!-- Main content -->
     <section class="content">
         <!-- Main content -->
-        <div class="container-fluid">
-            <h4>Projeto</h4>
-            <div class="row" style="background: #fff; border-top: 1px solid #dee2e6; border-bottom: 1px solid #dee2e6; color: black; margin-bottom: 150px">
-                <div class="col-md-12" style="padding-bottom: 50px;">
-                    <p>Sistema de gestão de controle e processus desenvolvido através do convenio Convênio 001/2019 PMJ/UESB uesb e prefeitura.</p>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card card-widget widget-user-2 shadow-sm">
+                    <!-- Add the bg color to the header using any of the bg-* classes -->
+                    <div class="widget-user-header bg-info">
+                        <div class="widget-user-image">
+                            <img class="img-circle elevation-2"
+                                src="http://www2.uesb.br/cpds/wp-content/uploads/2014/05/claudi3.png" alt="User Avatar">
+                        </div>
+                        <!-- /.widget-user-image -->
+                        <h3 class="widget-user-username">Claudia Ribeiro Santos Lopes</h3>
+                        <h5 class="widget-user-desc">Gerente de Projeto / Analista Sênior</h5>
+                    </div>
+                    <div class="card-footer p-0">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Doutora em Difusão do Conhecimento pela Universidade Federal da Bahia (FACED/UFBA)
+
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Professora da Universidade Estadual do Sudoeste da Bahia – UESB
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Coordenadora do Centro de Pesquisa e Desenvolvimento de Softwares – CPDS
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Coordenadora do Curso de Sistemas de Informação da UESB
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <!-- /.col -->
             </div>
 
-            <h4>Equipe</h4>
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="row">
-                        <div class="col-md-9">
-                            <a href="#">Claudia Ribeiro Santos Lopes</a>
-                            <p>Gerencia de Projeto / Analista Sênior</p>
+            <div class="col-md-4">
+                <div class="card card-widget widget-user-2 shadow-sm">
+                    <!-- Add the bg color to the header using any of the bg-* classes -->
+                    <div class="widget-user-header bg-info">
+                        <div class="widget-user-image">
+                            <img class="img-circle elevation-2"
+                                src="http://www2.uesb.br/cpds/wp-content/uploads/2017/11/foto-jessica.jpg"
+                                alt="User Avatar">
                         </div>
-                        <!-- /.col -->
+                        <!-- /.widget-user-image -->
+                        <h3 class="widget-user-username">JÉSSICA CERQUEIRA</h3>
+                        <h5 class="widget-user-desc">Scrum Master da Fábrica de Software</h5>
+                    </div>
+                    <div class="card-footer p-0">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Analista de Sistemas
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Desenvolvedora Web
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <!-- /.col -->
+            </div>
 
-                <div class="col-md-3">
-                    <div class="row">
-                        <div class="col-md-9">
-                            <a href="#">Jéssica Cerqueira</a>
-                            <p>Scrum Master e Desenvolvedor</p>
+            <div class="col-md-4">
+                <div class="card card-widget widget-user-2 shadow-sm">
+                    <!-- Add the bg color to the header using any of the bg-* classes -->
+                    <div class="widget-user-header bg-info">
+                        <div class="widget-user-image">
+                            <img class="img-circle elevation-2"
+                                src="http://www2.uesb.br/cpds/wp-content/uploads/2018/07/tailane.jpg" alt="User Avatar">
                         </div>
-                        <!-- /.col -->
+                        <!-- /.widget-user-image -->
+                        <h3 class="widget-user-username">TAILANE MAIA</h3>
+                        <h5 class="widget-user-desc">Discente do Curso Sistemas de Informação (UESB)</h5>
+                    </div>
+                    <div class="card-footer p-0">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Bolsista, atua em projetos desenvolvidos na Fábrica de Software
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <!-- /.col -->
-
-                <div class="col-md-3">
-                    <div class="row">
-                        <div class="col-md-9">
-                            <a href="https://linktr.ee/AndersonP.Almeida" target="_blank">Anderson P.Almeida</a>
-                            <p>Desenvolvedor</p>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </div>
-                <!-- /.col -->
-
-                <div class="col-md-3">
-                    <div class="row">
-                        <div class="col-md-9">
-                            <a href="#">Tailane Maia</a>
-                            <p>Desenvolvedora</p>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </div>
-                <!-- /.col -->
-
-                <div class="col-md-3">
-                    <div class="row">
-                        <div class="col-md-9">
-                            <a href="#">Túlio Calil</a>
-                            <p>Desenvolvedor</p>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </div>
-                <!-- /.col -->
-
-                <div class="col-md-3">
-                    <div class="row">
-                        <div class="col-md-9">
-                            <a href="#">Ronald Matos</a>
-                            <p>Desenvolvedor</p>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </div>
-                <!-- /.col -->
-
-                <div class="col-md-3">
-                    <div class="row">
-                        <div class="col-md-9">
-                            <a href="#">Willer Almeida</a>
-                            <p>Testes do Sistema e Documentação</p>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </div>
-                <!-- /.col -->
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card card-widget widget-user-2 shadow-sm">
+                    <!-- Add the bg color to the header using any of the bg-* classes -->
+                    <div class="widget-user-header bg-success">
+                        <div class="widget-user-image">
+                            <img class="img-circle elevation-2" src="https://imageshack.com/i/poDE39P4p"
+                                alt="User Avatar">
+                        </div>
+                        <!-- /.widget-user-image -->
+                        <h3 class="widget-user-username">RONALD MATOS</h3>
+                        <h5 class="widget-user-desc">Discente do Curso Sistemas de Informação (UESB)</h5>
+                    </div>
+                    <div class="card-footer p-0">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Bolsista, atua em projetos desenvolvidos na Fábrica de Software
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card card-widget widget-user-2 shadow-sm">
+                    <!-- Add the bg color to the header using any of the bg-* classes -->
+                    <div class="widget-user-header bg-success">
+                        <div class="widget-user-image">
+                            <img class="img-circle elevation-2" src="" alt="User Avatar">
+                        </div>
+                        <!-- /.widget-user-image -->
+                        <h3 class="widget-user-username">ANDERSON PEREIRA</h3>
+                        <h5 class="widget-user-desc">Discente do Curso Sistemas de Informação (UESB)</h5>
+                    </div>
+                    <div class="card-footer p-0">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Bolsista, atua em projetos desenvolvidos na Fábrica de Software
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card card-widget widget-user-2 shadow-sm">
+                    <!-- Add the bg color to the header using any of the bg-* classes -->
+                    <div class="widget-user-header bg-success">
+                        <div class="widget-user-image">
+                            <img class="img-circle elevation-2" src="" alt="User Avatar">
+                        </div>
+                        <!-- /.widget-user-image -->
+                        <h3 class="widget-user-username">TÚLIO CALIL</h3>
+                        <h5 class="widget-user-desc">Discente do Curso Sistemas de Informação (UESB)</h5>
+                    </div>
+                    <div class="card-footer p-0">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Bolsista, atua em projetos desenvolvidos na Fábrica de Software
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </section>
 </div>
 <script src="{{asset('plugins/jquery/jquery.min.js') }}"></script>
