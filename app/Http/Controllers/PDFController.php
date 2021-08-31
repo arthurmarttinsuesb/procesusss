e3722f624cc5441e0389c9b36f33b2deeba776e7
Guilherme Pereira <guiwalper@gmail.com>
2021-08-31 14:52:39 -0300

    Mudança para o documento ficar sem título

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -25,12 +25,10 @@ class PDFController extends Controller
                 <head>
                 <meta charset='utf-8'>
                 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-                <title>$modelo->titulo</title>
                 <!-- Tell the browser to be responsive to screen width -->
                 <meta name='viewport' content='width=device-width, initial-scale=1'>
                 </head>
                 <body>
-                    <h1>$modelo->titulo</h1>
                     <p>$modelo->conteudo</p>
                 </body>
                 </html>";
