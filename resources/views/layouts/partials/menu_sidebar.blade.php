f4f977d1eb86ed5ded03834ea6aefa6616d6f5f8
Guilherme Pereira <guiwalper@gmail.com>
2021-09-01 16:11:41 -0300

    Identificação do usuário logado

--- resources/views/layouts/partials/menu_sidebar.blade.php
+++ resources/views/layouts/partials/menu_sidebar.blade.php
@@ -2,12 +2,23 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/home" class="brand-link elevation-4">
-        <span class="logo-mini"><img src="{{url('img/icone-processus.png')}}" alt="PROCESSUS-ÍCONE"
-                style="width: 40px; height: 40px; padding: 4px;"></span>
-        <span class="brand-text">Processus</span>
+        <span class="logo-mini"><img src="{{url('img/icone-processus.png')}}" alt="PROCESSUS-ÍCONE" style="width: 40px; height: 40px; padding: 4px;"></span>
+        <span class="brand-text">Processus</span><br>
+
     </a>
+
+    <!-- p style="color:white;font-family:verdana;text-align:center;"> Bem vindo, {{ Auth::user()->nome }}!</p> -->
+
     <!-- Sidebar -->
     <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image" style="color:white;font-family:verdana;">
+                <i class="nav-icon fa fa-user"></i>
+            </div>
+            <div class="info">
+                <p class="d-block" style="color:white;font-family:verdana;">{{ Auth::user()->nome }}</p>
+            </div>
+        </div>
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -15,15 +26,15 @@
                with font-awesome or any other icon font library -->
                 <li class="nav-header">MENU</li>
                 @if (auth()->user()->status == 'Ativo')
-                   @foreach(Auth::user()->getRoleNames() as $nome)
-                        @if($nome=="administrador")
-                            @include('layouts.partials.menu.administrador')
-                        @elseif($nome=="funcionario")
-                            @include('layouts.partials.menu.funcionario')
-                        @elseif($nome=="cidadao")
-                            @include('layouts.partials.menu.cidadao')
-                        @endif
-                    @endforeach
+                @foreach(Auth::user()->getRoleNames() as $nome)
+                @if($nome=="administrador")
+                @include('layouts.partials.menu.administrador')
+                @elseif($nome=="funcionario")
+                @include('layouts.partials.menu.funcionario')
+                @elseif($nome=="cidadao")
+                @include('layouts.partials.menu.cidadao')
+                @endif
+                @endforeach
                 @else
                 <div class="alert alert-warning m-2">Por favor, ative o usuario.</div>
                 @endif
